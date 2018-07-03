a045129745048a29790bb4ec41251ed64d2f1282
Alexander Schelchere <ascellie@gmail.com>
2018-07-03 15:30:22 -0400

    Hello World!

---
+++ L_02_HelloWorld/src/Hello.java
@@ -0,0 +1,40 @@
+public class Hello {
+    public static void  main(String[] args) {
+
+        System.out.println("Hello, alex.");
+
+        int num1 = (10 + 5) + (2 * 7);
+        System.out.println(num1);
+
+        num1 += 2;
+        System.out.println(num1);
+
+        int num2 = 12;
+        int num3 = 6;
+
+        int myTotal = num1 + num2 + num3;
+
+        int num4 = 1000 - myTotal;
+        System.out.println("num 4 is " + num4);
+        System.out.println(num4);
+
+//        java.lang.String name = "alex";
+//        System.out.println(name);
+//        System.out.println("Get it, " + name + "!! YASSS!");
+//        int[] testScores = {70, 85, 90, 60, 95};
+//        System.out.println("testScore = " + testScores[0b0]);
+//        for (int i = 0; i < testScores.length; i++) {
+//            System.out.println("testScores = " + testScores[i]);
+//        }
+//        for (int i = 0; i <= 100; i++)
+////            if (((i % 3) == 0) && ((i % 5) == 0)) {
+////                System.out.println("FizzBuzz");
+////            } else if (i % 3 == 0) {
+////                System.out.println("Fizz");
+////            } else if (i % 5 == 0) {
+////                System.out.println("Buzz");
+////            } else {
+////                System.out.println(i);
+////            }
+    }
+}
